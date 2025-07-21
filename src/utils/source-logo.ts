f04f7cae93ff772b3eb8a1356797d4b67ab6d93a
Shachar <shachar147@gmail.com>
2025-07-21 14:52:16 +0300

    wip

--- src/utils/source-logo.ts
+++ src/utils/source-logo.ts
@@ -1,3 +1,24 @@
+export enum Source {
+  ASOS = 'asos',
+  TERMINALX = 'terminalx',
+  FACTORY54 = 'factory54',
+  ITAYBRANDS = 'itaybrands',
+  ZARA = 'zara',
+  STORY = 'story',
+  ONEPROJECTSHOP = 'oneprojectshop',
+  CHOZEN = 'chozen',
+  NIKE = 'nike',
+  JDSPORTS = 'jdsports',
+  GANT = 'gant',
+  RENUAR = 'renuar',
+  CASTRO = 'castro',
+  STOCKX = 'stockx',
+  TOMMY_HILFIGER = 'tommy hilfiger',
+  ALO_YOGA = 'alo yoga',
+  POLO_RALPH_LAUREN = 'polo ralph lauren',
+  STYLEFORRENT = 'styleforrent',
+}
+
 /**
  * Returns the logo URL for a given source name.
  * @param source string (e.g. 'asos.com')
@@ -8,41 +29,41 @@ export default function getSourceLogo(source?: string): string | undefined {
   const normalized = source.trim().toLowerCase();
   const S3_URL = 'https://buysearch.s3.eu-north-1.amazonaws.com/logos';
   switch (normalized) {
-    case 'asos':
+    case Source.ASOS:
       return `${S3_URL}/asos.png`;
-    case 'terminalx':
+    case Source.TERMINALX:
       return `${S3_URL}/terminalx.png`;
-    case 'factory54':
-      return `${S3_URL}/factory54.png`;
-    case 'itaybrands':
+    case Source.FACTORY54:
+      return `${S3_URL}/factory-54.png`;
+    case Source.ITAYBRANDS:
       return `${S3_URL}/itaybrands.png`;
-    case 'zara':
+    case Source.ZARA:
       return `${S3_URL}/zara.png`;
-    case 'story':
+    case Source.STORY:
       return `${S3_URL}/story.webp`;
-    case 'oneprojectshop':
+    case Source.ONEPROJECTSHOP:
       return `${S3_URL}/oneprojectshop.avif`;
-    case 'chozen':
-      return `${S3_URL}/Chozen.avif`
-    case 'nike':
-      return `${S3_URL}/nike.png`
-    case 'jdsports':
-      return `${S3_URL}/jd.svg`
-    case 'gant':
+    case Source.CHOZEN:
+      return `${S3_URL}/Chozen.avif`;
+    case Source.NIKE:
+      return `${S3_URL}/nike.png`;
+    case Source.JDSPORTS:
+      return `${S3_URL}/jd.svg`;
+    case Source.GANT:
       return `${S3_URL}/gant.png`;
-    case 'renuar':
+    case Source.RENUAR:
       return `${S3_URL}/renuar.png`;
-    case 'castro':
+    case Source.CASTRO:
       return `${S3_URL}/castro.webp`;
-    case 'stockx':
+    case Source.STOCKX:
       return `${S3_URL}/stockx.avif`;
-    case 'tommy hilfiger':
+    case Source.TOMMY_HILFIGER:
       return `${S3_URL}/tommy.svg`;
-    case 'alo yoga':
+    case Source.ALO_YOGA:
       return `${S3_URL}/alo.png`;
-    case 'polo ralph lauren':
+    case Source.POLO_RALPH_LAUREN:
       return `${S3_URL}/ralph-lauren.png`;
-    case 'styleforrent':
+    case Source.STYLEFORRENT:
       return `${S3_URL}/style-for-rent.webp`;
     // Add more sources here as needed
     default:
